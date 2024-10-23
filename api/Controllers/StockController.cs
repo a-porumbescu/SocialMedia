484fd3c80f30db128546b5e6f6343465e8b0d658
a-porumbescu <porumbesku280@gmail.com>
2024-10-23 22:36:08 +0300

    Update and Delete

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -10,7 +10,7 @@ namespace api.Controllers;
 public class StockController : ControllerBase
 {
     private readonly ApplicationDbContext _context;
-    public StockController(ApplicationDbContext context)
+    public StockController (ApplicationDbContext context)
     {
         _context = context;
     }
@@ -42,5 +42,46 @@ public class StockController : ControllerBase
         _context.Stocks.Add(stockModel);
         _context.SaveChanges();
         return CreatedAtAction(nameof(GetById), new { id = stockModel.StockId }, stockModel);
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+    {
+        var stockModel = _context.Stocks.FirstOrDefault(x => x.StockId == id);
+
+        if (stockModel == null)
+        {
+            return NotFound();
+        }
+        
+        stockModel.Symbol = updateDto.Symbol;
+        stockModel.CompanyName = updateDto.CompanyName;
+        stockModel.PurchasePrice = updateDto.PurchasePrice;
+        stockModel.LastDiv = updateDto.LastDiv;
+        stockModel.Industry = updateDto.Industry;
+        stockModel.MarketCap = updateDto.MarketCap;
+
+        _context.SaveChanges();
+        
+        return Ok(stockModel.ToStockDto());
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        var stockModel = _context.Stocks.FirstOrDefault(x => x.StockId == id);
+
+        if (stockModel == null)
+        {
+            return NotFound();
+        }
+        
+        _context.Stocks.Remove(stockModel);
+        
+        _context.SaveChanges();
+        
+        return NoContent();
     }
 }
---
+++ api/DTOs/Stock/UpdateStockRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace api.DTOs.Stock;
+
+public class UpdateStockRequestDto
+{
+    public string Symbol { get; set; } = string.Empty;
+    public string CompanyName { get; set; } = string.Empty;
+    public decimal PurchasePrice { get; set; }
+    public decimal LastDiv { get; set; }
+    public string Industry { get; set; } = string.Empty;
+    public long MarketCap { get; set; }
+}
